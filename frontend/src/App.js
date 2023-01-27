e41df14a7f4d3361d8a6f097e5ffc001e1d478ba
Shiva Menta <shiva.123.menta@gmail.com>
2023-01-27 13:56:11 -0500

    Add Nav-Group (No Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header.js'
 import SongDisplay from './components/SongDisplay.js'
@@ -5,7 +6,19 @@ import FeatureDropdowns from './components/FeatureDropdowns';
 import 'bootstrap/dist/css/bootstrap.css';
 import './components/component.css'
 
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import { BsMusicNoteList } from 'react-icons/bs';
+import { FaRecordVinyl } from 'react-icons/fa';
+
 function App() {
+  const [radioValue, setRadioValue] = useState('match')
+
+  const radios = [
+    { icon: <BsMusicNoteList/>, value: 'list' },
+    { icon: <FaRecordVinyl/>, value: 'match' }
+  ];
+
   return (
     <div className="App">
       <div className="view-container">
@@ -14,6 +27,21 @@ function App() {
         <SongDisplay/>
         <FeatureDropdowns/>
         <button className="match-button">match</button>
+        <ButtonGroup className="nav-button-group">
+          {radios.map((radio, idx) => (
+            <ToggleButton
+              key = {idx}
+              id={`radio${idx}`}
+              type="radio"
+              name="radio"
+              value={radio.value}
+              checked={radioValue === radio.value}
+              onChange={(e) => setRadioValue(e.currentTarget.value)}
+            >
+              {radio.icon}
+            </ToggleButton>
+          ))}
+        </ButtonGroup>
       </div>
     </div>
   );
