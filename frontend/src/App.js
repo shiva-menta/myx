e7f23a373e8cce8c952a0577fa01a3fb7e2d6423
Shiva Menta <shiva.123.menta@gmail.com>
2023-02-05 02:28:52 -0500

    Finish Functional Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header.js'
 import SongDisplay from './components/SongDisplay.js'
@@ -10,20 +10,37 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import { BsMusicNoteList } from 'react-icons/bs';
 import { FaRecordVinyl } from 'react-icons/fa';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 function App() {
-  const [radioValue, setRadioValue] = useState('match')
+  const [radioValue, setRadioValue] = useState('match');
+  const [searchState, setSearchState] = useState('');
 
   const radios = [
     { icon: <BsMusicNoteList/>, value: 'list' },
     { icon: <FaRecordVinyl/>, value: 'match' }
   ];
 
+  useEffect(() => {
+    let searchBox = document.querySelector(".song-search-container");
+		searchBox.addEventListener("mousedown", function() {
+			searchBox.style.backgroundColor = "rgba(0,0,0, 0.5)";
+		})
+		document.addEventListener("mousedown", function(event) {
+			if (event.target.closest(".song-search-container")) return;
+			searchBox.style.backgroundColor = "rgba(0,0,0, 0.35)";
+		})
+  })
+
   return (
     <div className="App">
       <div className="view-container">
         <Header/>
         <div className="page-title">[acapella match]</div>
+        <div className="song-search-container">
+          <AiOutlineSearch/>					
+          <input id="team-search" type="test" className="song-search-bar" placeholder="Search by title" onChange={(evt) => {setSearchState(evt.target.value)}}/>
+        </div>
         <SongDisplay/>
         <FeatureDropdowns/>
         <button className="match-button">match</button>
--- frontend/src/components/FeatureDropdowns.js
+++ frontend/src/components/FeatureDropdowns.js
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Stack from 'react-bootstrap/Stack'
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import DropdownList from '../jsons/dropdowns.json'
+import Form from 'react-bootstrap/Form';
 
 function FeatureDropdowns() {
     // Variables
     const dropdowns = [];
-    var counter;
+    var items = [];
+
+    // State Hooks
+    const [genreValue, setGenreValue] = useState("");
+    const [timeValue, setTimeValue] = useState("");
+    const [keyValue, setKeyValue] = useState("");
+    const [bpmValue, setBpmValue] = useState("");
 
     // Executed Code
-    for (var category in DropdownList) {
-        counter = 0;
-        const items = DropdownList[category].options.map((item) => 
-            <Dropdown.Item href={`#/action-${counter}`} key={item}>{item}</Dropdown.Item>
-        );
-        dropdowns.push(<DropdownButton className="feature-dropdown" title={category} key={category}>
-            {items}
-        </DropdownButton>);
-    }
+    items = DropdownList["Genre"].options.map((item) => 
+        <option key={item} value={item}>{item}</option>
+    );
+    dropdowns.push(<Form.Select className="feature-dropdown arrow-color">
+        <option>Genre</option>
+        {items}
+    </Form.Select>);
+
+    items = [];
+    items = DropdownList["Time"].options.map((item) => 
+        <option key={item} value={item}>{item}</option>
+    );
+    dropdowns.push(<Form.Select className="feature-dropdown">
+        <option>Time</option>
+        {items}
+    </Form.Select>);
+
+    items = [];
+    items = DropdownList["Key"].options.map((item) => 
+        <option key={item} value={item}>{item}</option>
+    );
+    dropdowns.push(<Form.Select className="feature-dropdown">
+        <option>Key</option>
+        {items}
+    </Form.Select>);
+
+    items = [];
+    items = DropdownList["BPM"].options.map((item) => 
+        <option key={item} value={item}>{item}</option>
+    );
+    dropdowns.push(<Form.Select className="feature-dropdown">
+        <option>BPM</option>
+        {items}
+    </Form.Select>);
+
+    
 
     // Render Function
     return (
