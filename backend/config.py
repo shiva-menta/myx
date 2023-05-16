7e3a2d46847b132a39a82278f41a321d30244d6a
Shiva Menta <shiva.123.menta@gmail.com>
2023-05-15 20:33:49 -0400

    V1 Refactor Deployment

---
+++ backend/config.py
@@ -0,0 +1,27 @@
+import os
+
+def get_env_variable(name):
+    try:
+        return os.environ.get(name) 
+    except KeyError:
+        message = "Expected environment variable '{}' not set.".format(name)
+        raise Exception(message)
+
+DB_FILE = "songs.db"
+
+pg_username = get_env_variable('POSTGRES_USER')
+pg_password = get_env_variable('POSTGRES_PASSWORD')
+pg_host = get_env_variable('POSTGRES_HOST')
+pg_port = get_env_variable('POSTGRES_PORT')
+pg_db = get_env_variable('POSTGRES_DB')
+redis_url = get_env_variable('REDIS_URL')
+backend_url = get_env_variable('BACKEND_URL')
+frontend_url = get_env_variable('FRONTEND_URL')
+database_url = get_env_variable('DATABASE_URL')
+
+DEV_DB = f'sqlite:///{DB_FILE}'
+
+# Testing Mode
+# PROD_DB = database_url or f'postgresql://{pg_username}:{pg_password}@{pg_host}:{pg_port}/{pg_db}'
+# Deployment Mode
+PROD_DB = database_url.replace('postgres://', 'postgresql://', 1)
