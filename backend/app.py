4cc7a2772f14ee2e30fe3ddd3cfbef3159edef3f
Shiva Menta <shiva.123.menta@gmail.com>
2024-09-28 16:34:45 -0400

    Print json data

--- backend/app.py
+++ backend/app.py
@@ -148,6 +148,7 @@ def callback():
   response = get_user_token(code)
   
   print(response.json())
+  
   user_access_token = response.json()['access_token']
   user_refresh_token = response.json()['refresh_token']
   expires_in = response.json()['expires_in']
