d1fd54af219cca6fc947ba758d257732044570e8
Shiva Menta <shiva.123.menta@gmail.com>
2024-09-28 23:08:07 -0400

    Final Deployment Fixes

--- backend/utils/spotify.py
+++ backend/utils/spotify.py
@@ -34,10 +34,6 @@ def get_spotify_app_token():
   })
 
 def get_user_token(code):
-  print(AUTH_URL)
-  print(code)
-  print(client_headers)
-  print(REDIRECT_URI)
   return session.post(AUTH_URL, 
     data={
       'grant_type': 'authorization_code',
